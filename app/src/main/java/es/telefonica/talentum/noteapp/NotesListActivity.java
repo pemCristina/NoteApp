76e1c55a567e6b3a523203166efdc9be005d1ed1
Cristina Perez <pem.cristina@gmail.com>
2017-01-12 11:38:38 +0100

    Launch detail from main finished

--- app/src/main/java/es/telefonica/talentum/noteapp/NotesListActivity.java
+++ app/src/main/java/es/telefonica/talentum/noteapp/NotesListActivity.java
@@ -1,9 +1,12 @@
 package es.telefonica.talentum.noteapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import es.telefonica.talentum.noteapp.model.NoteDetailActivity;
 
 public class NotesListActivity extends AppCompatActivity {
 
@@ -19,7 +22,7 @@ public class NotesListActivity extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
-
+// Android considera un giro de la pantalla como un cambio de configuración. Los cambios en el editText se pierden (ciclo de vida de las actividades
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
@@ -29,7 +32,8 @@ public class NotesListActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.menu_main_action_add_note) {
-            //TODO: me queda por abrir la pantalla de las notas
+            Intent i = new Intent(NotesListActivity.this, NoteDetailActivity.class);
+            startActivity(i);
             return true;
         }
 
